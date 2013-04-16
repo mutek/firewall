b7b63afc1c70d9de415d1c7303b0e52f40991b82
Luther Blissett <luther.blissett@localhost.localdomain>
2013-04-16 09:23:09 +0000

    aggiornamento Firewall www1 del 17 Aprile 2013

--- Header.sh
+++ Header.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
+#
+# Luca Cappelletti 2013
+#
+# GNU/GPL
+#
+# header iptable per mitigare questioni torride
+#
 
 iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
 wait
@@ -7,3 +14,10 @@ wait
 iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
 wait
 iptables -A INPIT -p tcp --tcp-flags ALL NONE -j DROP
+wait
+
+# la seguente sequenza svolge un throttling a 60 secondi
+iptables -I INPUT -p tcp --dport 22 -i eth0 -m state --state NEW -m recent --set
+wait
+iptables -I INPUT -p tcp --dport 22 -i eth0 -m state --state NEW -m recent --update --seconds 60 --hitcount 6 -j DROP
+wait
