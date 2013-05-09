80c37d2ede0b3646d743f4165ceab9005d00a9fd
Luther Blissett <luther.blissett@www.localhost.localdomain>
2013-05-09 16:09:46 +0000

    limitCpu.sh per controllare la cpu di Apache2 e MySQL

---
+++ bin/limitCpu.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+#
+# makeSwap.sh
+#
+# Copyleft 2013 Luca Cappelletti <luca.cappelletti@gmail.com>
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+# Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+#
+
+
+cpulimit -e mysqld -l 80 &
+cpulimit -e apache2 -l 80 &
