48d91d1be6b51f5d1e770009a361ac0e9ce724cb
Luther Blissett <luther.blissett@crimson>
2013-05-17 11:45:40 +0200

    little fixes e cosmetics

--- UpdateGithub.sh
+++ UpdateGithub.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env sh
 #
-# Luca Cappelletti 2013
-# GNU/GPL
-#
 # UpdateGithub.sh
+#
+#
+# Copyleft 2013 Luca Cappelletti <luca.cappelletti@gmail.com>
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+# Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
 #
 
 git config --global user.name "Luther Blissett"
-git config --global user.email luther.blissett@localdomain
+git config --global user.email luther.blissett@$(hostname)
 
 echo "Aggiungo i cambiamenti al database locale"
 git add .
